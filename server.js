36f9d92d36b19513936cbe120705aad9a4079cdb
Camilo Jimenez <jucajigue+developer@gmail.com>
2022-02-22 11:12:56 -0500

    Initial commit

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const bodyparser = require('body-parser');
 const employeeController = require('./controllers/employeeController');
 
 var app = express();
+
 app.use(bodyparser.urlencoded({
     extended: true
 }));
+
 app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
